4366666b41704e1b4b0af3e3be5622b651047c9c
tubalejasper8 <tubalejasper8@gmail.com>
2024-10-04 14:38:45 +0800

    Create Lamp.java

---
+++ Lamp.java
@@ -0,0 +1,14 @@
+class Lamp{
+
+	boolean isOn;
+	
+	void turnOn(){
+		isOn = true;
+		System.out.println("Light on? " + isOn);
+	}
+	
+	void turnOff(){
+		isOn = false;
+		System.out.println("Light on? " + isOn);
+	}
+}
