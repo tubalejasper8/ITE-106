95b82efd0bb8d2f146b8c86fb66be13427aab576
tubalejasper8 <tubalejasper8@gmail.com>
2024-10-04 15:00:10 +0800

    Create TextFileBufferedReader.java

---
+++ TextFileBufferedReader.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class TextFileBufferedReader {
+
+	public static void main(String[] args) throws Exception
+	{
+	
+		File file = new File("jasper.txt");
+		
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String st;
+		while ((st = br.readLine()) != null)
+		{
+			System.out.println(st);
+		}
+	}
+}
